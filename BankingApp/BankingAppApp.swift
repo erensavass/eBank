4a2bf36a9677155211f1a1d08c40f7b0a08bd053
Eren Savas <erensavas@icloud.com>
2024-09-03 10:15:17 +0300

    Initial Commit

---
+++ BankingApp/BankingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankingAppApp.swift
+//  BankingApp
+//
+//  Created by InterTech on 3.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct BankingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
